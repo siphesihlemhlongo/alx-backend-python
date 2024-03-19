f66d75f196d1a2814fcfcb1d8858402668e01a35
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2024-03-19 09:41:03 +0000

    python function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,9 +2,19 @@
 import asyncio
 from typing import List
 from time import time
-from .1-concurrent_coroutines import wait_n  # Importing wait_n from previous file
+from .1-concurrent_coroutines import wait_n, wait_random  # Importing wait_n and wait_random from previous file
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronous coroutine that spawns wait_random n times with the specified max_delay.
+    
+    Parameters:
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay in seconds.
+    
+    Returns:
+        List[float]: List of all the delays in ascending order.
+    """
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
     return sorted(delays)
